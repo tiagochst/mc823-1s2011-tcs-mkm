f1e3ff73517a6f2dc6e7c1f9e37ea1f5107ce2a6
tiagochst <tiagochst@fe639ff6-e610-6a5c-e408-ba0ff61bacb9>
2011-03-24 14:29:46 +0000

    aumentamos maxdatasize do cliente

--- trunk/lab1/code/client.c
+++ trunk/lab1/code/client.c
@@ -16,7 +16,7 @@
 
 #define PORT "3490" // the port client will be connecting to 
 
-#define MAXDATASIZE 100 // max number of bytes we can get at once 
+#define MAXDATASIZE 1000 // max number of bytes we can get at once 
 
 // get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
@@ -85,7 +85,7 @@ int main(int argc, char *argv[])
 
     buf[numbytes] = '\0';
 
-    printf("client: received '%s'\n",buf);
+    printf("%s\n",buf); //client received
 
     close(sockfd);
 

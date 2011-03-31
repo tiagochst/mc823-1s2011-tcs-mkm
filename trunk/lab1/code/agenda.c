41d22960ea49d7479881b12dbaa0a6255220cd97
tiagochst <tiagochst@fe639ff6-e610-6a5c-e408-ba0ff61bacb9>
2011-03-31 14:59:07 +0000

    inserindo compromisso

--- trunk/lab1/code/agenda.c
+++ trunk/lab1/code/agenda.c
@@ -25,7 +25,67 @@ void user_destroy(User *u) {
   }
 }
 
-int angenda_vazia(User *a) {
+int agenda_vazia(User *a) {
   return (a->tasks == NULL)?(1):(0);
 }
 
+int set_task(int dia,int hora,char task[], User u){
+  int cmp;
+  Agenda *a,*next,*ant;
+  Agenda *newTask = (Agenda *) malloc(sizeof(Agenda));
+  newTask.dia=dia;
+  newTask.hora=hora;
+  strcpy(newTask.tasks,task);
+  newTask.next=Null;
+
+  next=u->tasks;
+  /*Já é a menor?*/
+  if(compData(newTasks,next)==1){
+    newTask->next=u->tasks->next;
+    u->tasks=newTask;
+    return 1; 
+  }
+
+  /* insere ordenado usando insertion sort */
+  for (a = next; a != NULL; a = next) {
+    cmp=compData(newTasks,a); 
+    if(cmp==-1){
+      ant=a;
+      next = a->next;
+    }
+    else if(cmp==1){
+      newTask.next = a;
+      ant->next=newTask;
+    }      
+    else{
+      return 0;
+    }
+  }
+    /*ultimo compromisso*/  
+  ant=newTask;
+  return 1;
+}
+
+/* 
+   Retorna:
+   1 se dia do novo compromisso é menor
+   -1 se dia do novo compromisso é maior
+   0 se dia do novo compromisso tem mesmo  horário
+*/
+int compData(Agenda *newTasks,Agenda *tasks){
+
+  if(newTasks.dia<tasks.dia)
+    return 1;
+  else if(newTasks.dia>tasks.dia)
+    return -1;
+  else if (newTasks.hora<tasks.hora)	
+    return 1;
+  else if (newTasks.hora>tasks.hora)	
+    return -1;
+  else if (newTasks.min<tasks.min)	
+    return 1;
+  else if (newTasks.min>tasks.min)	
+    return 1;
+
+  return 0;
+}

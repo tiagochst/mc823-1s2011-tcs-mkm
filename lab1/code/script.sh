c9c578b045a51696c4636fac3489aeb31c491aaa
marcelokmats <marcelokmats@fe639ff6-e610-6a5c-e408-ba0ff61bacb9>
2011-04-12 19:29:31 +0000

    entradas para cada operacao

--- lab1/code/script.sh
+++ lab1/code/script.sh
@@ -2,5 +2,39 @@
 
 for((i=0;i<100;i++))
 do
-    ./c localhost < input2
+    mv asd asd~
+    ./c localhost < ./entradas/input1
 done
+
+mv *.dat 1
+
+for((i=0;i<100;i++))
+do
+    cp asd~ asd
+    ./c localhost < ./entradas/input2
+done
+
+mv *.dat 2
+
+for((i=0;i<100;i++))
+do
+    ./c localhost < ./entradas/input3
+done
+
+mv *.dat 3
+
+
+for((i=0;i<100;i++))
+do
+    ./c localhost < ./entradas/input4
+done
+
+mv *.dat 4
+
+
+for((i=0;i<100;i++))
+do
+    ./c localhost < ./entradas/input5
+done
+
+mv *.dat 5

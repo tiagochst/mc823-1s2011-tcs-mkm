e116db134249d7a5e62feb833e1bae3c118d0caf
tiagochst <tiagochst@fe639ff6-e610-6a5c-e408-ba0ff61bacb9>
2011-05-03 14:06:26 +0000

    Codigo do relatorio

---
+++ lab2/code/rel/agenda.h
@@ -0,0 +1,60 @@
+#ifndef AGENDA_H_
+#define AGENDA_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include "bd.h" 
+
+#define PORT "35555"  // the port users will be connecting to
+#define MAXDATASIZE 1000
+#define BACKLOG 10     // how many pending connections queue will hold
+
+/*Comaracao de compromissos*/
+#define ANTES 1     // how many pending connections queue will hold
+#define DEPOIS -1     // how many pending connections queue will hold
+#define SIMULTANEO 0     // how many pending connections queue will hold
+
+
+typedef struct agenda {
+  struct agenda *next;
+  int dia;
+  int hora, min;
+  char task[256];
+} Agenda;
+
+
+typedef struct user {
+  struct agenda *tasks;
+  char name[20];
+} User;
+
+/* Funcoes */
+void menu(int new_fd, struct sockaddr_storage their_addr);
+void menu2(int new_fd, struct sockaddr_storage their_addr, User *user);
+void sendStr(int sockfd, char str[]);
+void sendMsg(int new_fd, char str[],struct sockaddr_storage their_addr);
+int leOpcao(struct sockaddr_storage their_addr, int sockfd);
+void leString(struct sockaddr_storage their_addr, int sockfd , char string[]);
+User * agenda_init(char nome[]);
+void user_destroy(User *u);
+int agenda_vazia(User *a);
+int compData(Agenda *newTasks,Agenda *tasks);
+int set_task(int dia,int hora, int min,char task[], User *u);
+Agenda * task_init(int dia,int hora, int min,char task[]);
+int verMes(int new_fd, User *u,struct sockaddr_storage their_addr);
+int verDia(int new_fd, User *u, int dia,struct sockaddr_storage their_addr);
+int verHora(int new_fd, User *u, int dia, int hora,struct sockaddr_storage their_addr);
+int delTask( User *u, char nome[]);
+void cpComp(Agenda *a, char comp[]);
+#endif /*CONJUNTO_H_*/
+
---
+++ lab2/code/rel/bd.h
@@ -0,0 +1,9 @@
+#ifndef BD_H_
+#define BD_H_
+
+int findUser(char nome[], char pwd[]);
+int newUser(char nome[], char senha[]);
+int loadCal();
+int saveCal();
+
+#endif /* BD_H_ */

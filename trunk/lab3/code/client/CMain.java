b7cb485d0801f02048474b291e9c980e741efca6
tiagochst <tiagochst@fe639ff6-e610-6a5c-e408-ba0ff61bacb9>
2011-05-12 15:30:27 +0000

    Iniciando client

---
+++ trunk/lab3/code/client/CMain.java
@@ -0,0 +1,121 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class CMain {
+	
+    private CMain(){}
+	
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+	Client user = new Client();
+	BufferedReader leitor = new BufferedReader(new InputStreamReader(System.in));
+	int opSelect;
+	IServer stub = null;
+
+	/* Recebo o nome/ip do servidor para a conexão */		
+	String host = (args.length < 1) ? null : args[0];
+	try {
+	    Registry registry = LocateRegistry.getRegistry(host);
+	    stub = (IServer) registry.lookup("MC823Server");
+	} catch (Exception e) {
+	    System.err.println("Client exception: " + e.toString());
+	    e.printStackTrace();
+	}
+
+	/* Inicia com usuário não cadastrado*/	
+	user.NonUserMenu();
+	opSelect = 0;
+	System.out.print("\n    Digite a opçao desejada: ");
+	
+	for(;;){
+	    try {
+		String inBuffer = leitor.readLine();
+		if(inBuffer.length() == 1){
+		    opSelect = inBuffer.charAt(0);
+		}
+	    } catch (Exception e) {
+		System.err.println("Read exception: " + e.toString());
+	    }
+	    
+	    if ((opSelect < '1') || (opSelect > '3'))
+		System.out.println("\n    Opcao invalida, digite a opcao novamente.");
+	    else
+		break;
+	
+	    switch(opSelect){
+	    case '1':
+		user.Login();
+		break;
+	    case '2':
+		user.NewCal();
+		break;
+	    case '3':
+		try{
+		    leitor.close();
+		} catch (Exception e){
+		    System.err.println("Read exception: " + e.toString());
+		}
+		System.exit(0);
+	    default:
+	    }
+	    if(1)
+		break;
+	}
+
+	for(;;){
+	    user.UserMenu();
+	    
+	    //Leitura da operacao com blindagem de erros
+	    for(;;){
+		opSelect = 0;
+		System.out.print("\n    Digite a opçao desejada: ");
+		try {
+		    String inBuffer = leitor.readLine();
+		    if(inBuffer.length() == 1){
+			opSelect = inBuffer.charAt(0);
+		    }
+		} catch (Exception e) {
+		    System.err.println("Read exception: " + e.toString());
+		}
+		
+		if ((opSelect < '1') || (opSelect > '6'))
+		    System.out.println("\n    Opcao invalida, digite a opcao novamente.");
+		else
+		    break;
+		
+	    }
+	    
+	    switch(opSelect){
+	    case '1':
+		user.NewComp(stub);
+		break;
+	    case '2':
+		user.DelComp(stub);
+		break;
+	    case '3':
+		user.ShowHour(stub);
+		break;
+	    case '4':
+		user.ShowDay(stub);
+		break;
+	    case '5':
+		user.ShowMonth(stub);
+		break;
+	    case '6':
+		try{
+		    leitor.close();
+		} catch (Exception e){
+		    System.err.println("Read exception: " + e.toString());
+		}
+		System.exit(0);
+	    default:
+	    }
+	}
+	
+    }
+}
---
+++ trunk/lab3/code/client/Opt.java
@@ -0,0 +1,48 @@
+import java.io.Serializable;
+
+/* Armazena as informaçoes necessarias na classe Opr */
+public class Opr implements Serializable{
+	private int operacao;
+	private int dia;
+	private int hora;
+	private String string;
+	private String login;
+	private String password;
+
+	public int getDia() {
+		return dia;
+	}
+	public void setDia(int dia) {
+		this.dia = dia;
+	}
+	public int getHora() {
+		return hora;
+	}
+	public void setHora(int hora) {
+		this.hora = hora;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void getPassword(String wassword) {
+		this.password = password;
+	}
+	public int getOperacao() {
+		return operacao;
+	}
+	public void setOperacao(int operacao) {
+		this.operacao = operacao;
+	}
+	public String getString() {
+		return string;
+	}
+	public void setString(String string) {
+		this.string = string;
+	}
+}

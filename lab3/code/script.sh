65f9605bb8fc4238e80322f16328c9acbdbfad5b
marcelokmats@gmail.com <marcelokmats@gmail.com@fe639ff6-e610-6a5c-e408-ba0ff61bacb9>
2011-05-16 18:43:47 +0000

    script que mata processo

---
+++ lab3/code/script.sh
@@ -0,0 +1,15 @@
+ps -u | grep $1 > lixo
+cat lixo | while read linha
+do
+    cont=0
+    echo $linha
+    for palavra in $linha
+    do
+	let cont++
+	if [ $cont -eq 2 ]
+	then
+	    kill $palavra
+	fi
+    done
+done
+rm lixo
